813674729b643f9d88a92461f1ea48eb3c634dd9
Napassorn Jensupakarn <napassorn.jspk@gmail.com>
2026-06-28 17:12:00 +0700

    add quote

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function toggleDark(){
     document.body.classList.toggle("dark");
 }
+
+fetch("https://dummyjson.com/quotes/random")
+    .then(res => res.json())
+    .then(data => {
+         const text =
+            `💬 คำคมประจำวัน: "${data.quote}" — ${data.author}`;
+        document.getElementById("quote").innerText = text;
+    })
+    .catch(err => {
+        document.getElementById("quote").innerText = "โหลดข้อมูลไม่สำเร็จ";
+        console.log(err);
+    });
